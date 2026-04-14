44799c50cb95afe58d91cfbe577a8f9e753dc76b
David Brett <davidbrett2002@gmail.com>
2026-04-14 19:57:24 +0200

    flowLauncherPlugins: add subtitle 'Flow Launcher Plugin'

--- src/data/flowLauncherPlugins.ts
+++ src/data/flowLauncherPlugins.ts
@@ -3,6 +3,7 @@ import type { Project } from '../types/project';
 export const flowLauncherPlugins: Project[] = [
 	{
 		name: 'App Audio Manager',
+		subtitle: 'Flow Launcher Plugin',
 		description: 'Control the volume and mute state of individual applications directly from Flow Launcher',
 		icon: 'https://cdn.jsdelivr.net/gh/DavidGBrett/Flow.Launcher.Plugin.AppAudioManager@main/Flow.Launcher.Plugin.AppAudioManager/Assets/PluginIcon.png',
 		links: [
@@ -12,6 +13,7 @@ export const flowLauncherPlugins: Project[] = [
 	},
 	{
 		name: 'Virtual Desktops',
+		subtitle: 'Flow Launcher Plugin',
 		description: 'Switch between Virtual Desktops using Flow Launcher',
 		icon: 'https://cdn.jsdelivr.net/gh/DavidGBrett/Flow.Launcher.Plugin.VirtualDesktops@master/assets/main_icon.png',
 		links: [
@@ -21,6 +23,7 @@ export const flowLauncherPlugins: Project[] = [
 	},
 	{
 		name: 'Query Groups',
+		subtitle: 'Flow Launcher Plugin',
 		description: 'A plugin for creating custom groups of queries',
 		icon: 'https://cdn.jsdelivr.net/gh/DavidGBrett/Flow.Launcher.Plugin.QueryGroups@main/Flow.Launcher.Plugin.QueryGroups/Assets/icon.png',
 		links: [
@@ -30,6 +33,7 @@ export const flowLauncherPlugins: Project[] = [
 	},
 	{
 		name: 'Time In',
+		subtitle: 'Flow Launcher Plugin',
 		description: 'Get the current time in different timezones around the world',
 		icon: 'https://cdn.jsdelivr.net/gh/DavidGBrett/Flow.Launcher.Plugin.TimeIn@main/Flow.Launcher.Plugin.TimeIn/Assets/PluginIcon.png',
 		links: [
